7126b54e2b6cae72660c7e8cc4a7f37b5358aa37
CHazey <chazelton3@gatech.edu>
2022-04-17 14:42:14 -0400

    Hotfix
    All three main methods are working in basic form
    
    Removed Helloworld

--- main.lua
+++ main.lua
@@ -1,34 +1,34 @@
-SLASH_HELLO1 = "/helloworld"
-
-
-local playerName = UnitName("player")
-
-local function helloWorldHandler(name)
-
-    local nameExists = string.len(name) > 0
-
-    if(nameExists) then
-        message("Hello, " .. name .. "!")
-    else
-        local playerName = UnitName("player")
-
-        message("Hello, " .. playerName .. "!")
-    end
-end
-
-
-SlashCmdList["HELLO"] = helloWorldHandler
+playerName = UnitName("player")
 
 SLASH_TRINK1 = "/trinket"
 
-local function trinketHandler(name)
-    local trinket1 = {}
-    local playerName = UnitName("player")
+local function trinketHandler()
     local start, duration, enable = GetInventoryItemCooldown(playerName, 13)
-    message(start .. " " .. duration .. " " .. enable)
+
+    local cooldown = duration-(GetTime() - start);
+
+    message(cooldown)
 end
 
 SlashCmdList["TRINK"] = trinketHandler
 
+SLASH_HEALTH1 = "/health"
 
+local function healthHandler()
+    local health = UnitHealth(playerName)
+    local maxHealth = UnitHealthMax(playerName)
+
+    message(health)
+end
+
+SlashCmdList["HEALTH"] = healthHandler
+
+SLASH_DEBUFF1 = "/debuff"
+
+local function debuffHandler()
+    local name, rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, shouldConsolidate, spellId = UnitDebuff(playerName, 1)
+    message(name)
+end
+
+SlashCmdList["DEBUFF"] = debuffHandler
 
